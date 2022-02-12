a9c955a69b452154cbda7744e8b1acf3f3e9b45d
SHI3DO <61821471+SHI3DO@users.noreply.github.com>
2022-02-13 02:12:04 +0900

    pawn rules

--- chess.py
+++ chess.py
@@ -2,8 +2,6 @@ import createboard
 import heuristic
 import chessrules
 
-turncount = 0
-
 # 보드 만들기
 playboard = createboard.create()
 
@@ -12,6 +10,5 @@ for i in range(8):
 
 print(heuristic.calculate(playboard))
 
-print(chessrules.wheretogo(playboard, 'white', 'pawn', '8'))
-print(chessrules.wheretogo(playboard, 'white', 'pawn', '5'))
-print(chessrules.wheretogo(playboard, 'white', 'pawn', '6'))
+print(chessrules.wheretogo(playboard, 'black', 'pawn', '2'))
+
--- chessrules.py
+++ chessrules.py
@@ -10,22 +10,89 @@ def wheretogo(array: list, color: str, name: str, identifier: str):
                 break
 
     usearray = []
+    # 폰일 경우
     if name == 'pawn':
-        if y == 0:
-            usearray.append(array[x][y + 1])
-        elif y == 7:
-            usearray.append(array[x][y - 1])
+        # 흰색 말이라면
+        if color == 'white':
+            # x가 1일때가 처음 시작이라 2칸을 이동할 수 있을 때
+            if x == 1:
+                if y == 0:
+                    usearray.append(array[x + 1][y])
+                    usearray.append(array[x + 2][y])
+                elif y == 7:
+                    usearray.append(array[x + 1][y])
+                    usearray.append(array[x + 2][y])
+                else:
+                    usearray.append(array[x + 1][y])
+                    usearray.append(array[x + 2][y])
+
+            # 처음 시작이 아닌 나머지 경우
+            else:
+                if y == 0:
+                    usearray.append(array[x + 1][y])
+                elif y == 7:
+                    usearray.append(array[x + 1][y])
+                else:
+                    usearray.append(array[x + 1][y])
+
+            # 먹을 수 있는 경우
+            # 적이 있는지 확인해서 경우의 수에 추가
+            if y == 0:
+                if array[x + 1][y + 1] != '0:0':
+                    if str(array[x + 1][y + 1]).split(':')[0] != 'white':
+                        usearray.append(array[x + 1][y + 1])
+            elif y == 7:
+                if array[x + 1][y - 1] != '0:0':
+                    if str(array[x + 1][y - 1]).split(':')[0] != 'white':
+                        usearray.append(array[x + 1][y - 1])
+            else:
+                if array[x + 1][y - 1] != '0:0':
+                    if str(array[x + 1][y - 1]).split(':')[0] != 'white':
+                        usearray.append(array[x + 1][y - 1])
+                if array[x + 1][y + 1] != '0:0':
+                    if str(array[x + 1][y + 1]).split(':')[0] != 'white':
+                        usearray.append(array[x + 1][y + 1])
+
+        elif color == 'black':
+            # x가 1일때가 처음 시작이라 2칸을 이동할 수 있을 때
+            if x == 6:
+                if y == 0:
+                    usearray.append(array[x - 1][y])
+                    usearray.append(array[x - 2][y])
+                elif y == 7:
+                    usearray.append(array[x - 1][y])
+                    usearray.append(array[x - 2][y])
+                else:
+                    usearray.append(array[x - 1][y])
+                    usearray.append(array[x - 2][y])
+
+            # 처음 시작이 아닌 나머지 경우
+            else:
+                if y == 0:
+                    usearray.append(array[x - 1][y])
+                elif y == 7:
+                    usearray.append(array[x - 1][y])
+                else:
+                    usearray.append(array[x - 1][y])
+
+            if y == 0:
+                if array[x - 1][y + 1] != '0:0':
+                    if str(array[x - 1][y + 1]).split(':')[0] != 'black':
+                        usearray.append(array[x - 1][y + 1])
+            elif y == 7:
+                if array[x - 1][y - 1] != '0:0':
+                    if str(array[x - 1][y - 1]).split(':')[0] != 'black':
+                        usearray.append(array[x - 1][y - 1])
+            else:
+                if array[x - 1][y - 1] != '0:0':
+                    if str(array[x - 1][y - 1]).split(':')[0] != 'black':
+                        usearray.append(array[x - 1][y - 1])
+                if array[x - 1][y + 1] != '0:0':
+                    if str(array[x - 1][y + 1]).split(':')[0] != 'black':
+                        usearray.append(array[x - 1][y + 1])
         else:
-            usearray.append(array[x][y-1])
-            usearray.append(array[x][y+1])
+            pass
 
-        if x == 0:
-            usearray.append(array[x + 1][y])
-        elif x == 7:
-            usearray.append(array[x-1][y])
-        else:
-            usearray.append(array[x-1][y])
-            usearray.append(array[x+1][y])
+        # if turncount == 0:
 
-
-    return x, y, usearray
+    return usearray
