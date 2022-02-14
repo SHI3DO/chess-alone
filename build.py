408b6e801fcade8574895d5df0eaec4ed6517c6c
SHI3DO <61821471+SHI3DO@users.noreply.github.com>
2022-02-15 00:34:24 +0900

    gui

--- build.py
+++ build.py
@@ -1,5 +1,14 @@
+import os
+import shutil
 import subprocess
 
 
+if os.path.isdir('./dist'):
+    shutil.rmtree('./dist')
+
 subprocess.run('pip freeze > requirements.txt', shell=True)
-subprocess.run('pyinstaller --noconfirm --onefile --console  "D:/Github/chess-ai-python-raspberrypi/chess.py"', shell=True)
+subprocess.run('pyinstaller --noconfirm --onefile --windowed --icon '
+               '"D:/Github/chess-ai-python-raspberrypi/src/chess-ai-python-raspberrypi-logo.ico"  '
+               '"D:/Github/chess-ai-python-raspberrypi/chess-ai-python-raspberrypi-gui-shi3do.py"', shell=True)
+shutil.copytree('./src', './dist/src')
+print('done')
