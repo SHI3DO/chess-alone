a4fc81b668d0bf230681d8f4a659087357be1b26
SHI3DO <61821471+SHI3DO@users.noreply.github.com>
2022-02-14 22:52:38 +0900

    pyinstaller

--- build.py
+++ build.py
@@ -2,3 +2,4 @@ import subprocess
 
 
 subprocess.run('pip freeze > requirements.txt', shell=True)
+subprocess.run('pyinstaller --noconfirm --onefile --console  "D:/Github/chess-ai-python-raspberrypi/chess.py"', shell=True)
