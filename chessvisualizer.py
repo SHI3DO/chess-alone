616e7386433f3c1aa7372fafdc39aefa1474a1a4
SHI3DO <61821471+SHI3DO@users.noreply.github.com>
2022-02-15 19:41:16 +0900

    no more rect + padding

--- chessvisualizer.py
+++ chessvisualizer.py
@@ -7,7 +7,7 @@ def visual(array):
     print(width, height)
 
     Size = 150
-    CSize = 140
+    CSize = 130
     SizeW = Size * width
     SizeH = Size * height
     canvas = Image.new("RGBA", (SizeW, SizeH), color="#ffffff")
@@ -24,21 +24,6 @@ def visual(array):
     whiterook = Image.open('./src/whiterook.png').resize((CSize, CSize))
     whitepawn = Image.open('./src/whitepawn.png').resize((CSize, CSize))
     x = Image.open('./src/x.png').resize((CSize, CSize))
-
-    rect = ImageDraw.Draw(canvas)
-    rect.rectangle(((SizeW * 1 / 35, SizeH * 1 / 35), (SizeW * 34 / 35, SizeH * 34 / 35)), outline=(0, 0, 0), width=4)
-
-    for i in range(0, width):
-        drawx = (SizeW * 33 / 35) / width
-        linex = ImageDraw.Draw(canvas)
-        line = ((SizeW * 1 / 35 + drawx * i, SizeH * 1 / 35), (SizeW * 1 / 35 + drawx * i, SizeH * 34 / 35))
-        linex.line(line, fill="black")
-
-    for i in range(0, height):
-        drawy = (SizeH * 33 / 35) / height
-        liney = ImageDraw.Draw(canvas)
-        line = ((SizeW * 1 / 35, SizeH * 1 / 35 + drawy * i), (SizeW * 34 / 35, SizeH * 1 / 35 + drawy * i))
-        liney.line(line, fill="black")
 
     for i in range(0, width):
         for j in range(0, height):
