2a8051ff98ea4c819ed10f76824c35e502e18087
SHI3DO <61821471+SHI3DO@users.noreply.github.com>
2022-05-21 20:38:49 +0900

    S

--- chess-alone-deploy/chess-alone.cmd
+++
@@ -1,6 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'chess-alone.exe' with Python DLL being found.
-set PATH=c:\users\shi3do\ANACON~1;%PATH%
-set PYTHONHOME=c:\users\shi3do\ANACON~1
-"%~dp0.\chess-alone.exe" %*
